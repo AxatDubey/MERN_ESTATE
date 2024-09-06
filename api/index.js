2f0a49d174c0d8b34df1486bbcf17c3573d2546d
AxatDubey <114806317+AxatDubey@users.noreply.github.com>
2024-09-06 18:08:50 +0530

    create signup api rout

--- api/index.js
+++ api/index.js
@@ -2,15 +2,17 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './route/User.rout.js';
+import authRouter from './route/auth.rout.js';
 dotenv.config();
 
 
 mongoose.connect(process.env.MONGO ).then(()=>{
      console.log('connected to MongoDB')
 }).catch((err)=>{
-     console.log(err);
+     console.log(err);0
 })
 const app = express();
+app.use(express.json());
 
 app.listen(3000 , ()=>{
      console.log("server is runnign on port 3000");
@@ -18,3 +20,4 @@ app.listen(3000 , ()=>{
 );
 
 app.use('/api/user' , userRouter)
+app.use('/api/auth' , authRouter)
